118bb9b06e5c70249a947ed911e9045581fe02a1
yungcat <yungcatx@gmail.com>
2018-10-04 19:17:09 +0500

    oh i almost finished invorse

--- src/App.js
+++ src/App.js
@@ -1,132 +1,175 @@
 import React from 'react';
-
 import {withStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField'
-import Divider from '@material-ui/core/Divider'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
 
 import RestoreIcon from '@material-ui/icons/Restore';
-import ExploreIcon from '@material-ui/icons/Explore'
-import AddIcon from '@material-ui/icons/Add'
+import ExploreIcon from '@material-ui/icons/Explore';
+import AddIcon from '@material-ui/icons/Add';
 
 import {chats, messages} from './mock-data'
 
-const styles = theme => ({
-  root: {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    backgroundColor: theme.palette.background.default,
-  },
 
-  appBar: {
-    position: 'fixed',
-    width: `calc(100% - 320px)`,
+const styles = theme => ({
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: '320px 1fr',
+    gridTemplateAreas: `
+                    'header appbar'
+                    'chatlist chatlayout'
+                    'bottomnavigation input'
+    `,
+    backgroundColor: theme.palette.background.default,
+
+    "& div:nth-child(even)": {
+      // backgroundColor: "red",
+    },
+    "& div:nth-child(odd)": {
+      // backgroundColor: "green",
+    },
+
+
   },
-  drawerPaper: {
-    position: 'relative',
+  drawerContainer: {
+    gridArea: 'header',
     height: '100%',
-    width: 320,
+    width: '320px',
+  },
+  appBarContainer: {
+    gridArea: 'appbar',
+
+  },
+  chatsListContainer: {
+    gridArea: 'chatlist',
+    width: '320px',
+    height: '100%',
+    overflow: 'scroll',
+    whiteSpace: 'nowrap',
+  },
+  chatLayoutContainer: {
+    gridArea: 'chatlayout',
+    gridTemplateRows: '1fr',
+    height: '100%',
+    width: '100%',
+    display: 'grid',
+    overflow: 'hidden',
+
+  },
+  message: {
+    display: 'grid',
+    justifySelf: 'right',
+    maxWidth: '70%',
+    minWidth: '10%',
+  },
+  senderIsMe: {
+    display: 'grid',
+    justifySelf: 'left',
+    maxWidth: '70%',
+    minWidth: '10%',
+  },
+  bottomNavigationContainer: {
+    gridArea: 'bottomnavigation',
+    position: 'fixed',
+    bottom: 0
+  },
+  inputContainer: {
+    gridArea: 'input',
+    position: 'fixed',
+    bottom: 0
+  },
+  AppBar: {
+    width: `calc(100% - 320px)`,
   },
   drawerHeader: {
     ...theme.mixins.toolbar,
-    paddingLeft: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-  },
-  ChatsList: {
-    height: 'calc(100% - 56px)',
-    overflowY: 'scroll'
-  },
-  addButton: {
-    position: 'absolute',
-    left: 'auto',
-    right: theme.spacing.unit * 3,
-    bottom: theme.spacing.unit * 3 + 40
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
   },
   bottomNav: {
-    position: 'absolute',
-    bottom: '10px'
-  },
-  Layout: {
-
-  },
+    width: '320px'
+  }
 });
 
-class PermanentDrawer extends React.Component {
+class App extends React.Component {
+
   render() {
     const {classes} = this.props;
-
+    const chatLists = chats.map((chat, index) => (
+      <ListItem key={index} button>
+        <Avatar>{chat.title && chat.title[0]}</Avatar>
+        <ListItemText primary={chat.title}/>
+      </ListItem>
+    ));
+    const messageLists = messages && messages.map((message, index) => (
+      <div key={index} className={(message.sender === 'me' ? classes.senderIsMe : classes.message)}>
+        <Paper>
+          <Typography variant="caption">
+            {message.sender}
+          </Typography>
+          <Typography variant="body1">
+            {message.content}
+          </Typography>
+        </Paper>
+      </div>
+    ));
 
     return (
-      <div className={classes.root}>
-        <div>
-
+      <div className={classes.grid}>
+        <div className={classes.drawerContainer}>
+          <div className={classes.drawerHeader}>
+            <TextField
+              fullWidth
+              margin="normal"
+              placeholder="Search chats..."
+            />
+          </div>
+          <Divider/>
+        </div>
+        <div className={classes.appBarContainer}>
           <AppBar
-            position="absolute"
-            className={classes.appBar}
-          >
+            position="absolute" className={classes.AppBar}>
             <Toolbar>
               <Typography variant="title" color="inherit" noWrap>
-                React Chat
+                Yungcat React Chat
               </Typography>
             </Toolbar>
           </AppBar>
-
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-          >
-
-            <div className={classes.drawerHeader}>
-              <TextField
-                fullWidth
-                margin="normal"
-                placeholder="Search chats..."
-              />
-            </div>
-            <Divider />
-
-            <List className={classes.ChatsList}>
-              {chats.map((chat, index) => (
-                <ListItem key={index} button>
-                  <Avatar>{chat.title[0]}</Avatar>
-                  <ListItemText primary={chat.title}/>
-                </ListItem>
-              ))};
-            </List>
-
-            <Button className={classes.addButton}
-            variant="fab"
-            aria-label="add"
-            color='primary'>
-              <AddIcon />
-            </Button>
-            <BottomNavigation showLabels>
-              <BottomNavigationAction label="My Chats" icon={<RestoreIcon/>}/>
-              <BottomNavigationAction label="Explore" icon={<ExploreIcon/>}/>
-            </BottomNavigation>
-          </Drawer>
-          <main className={classes.Layout}>
-          
-          </main>
+        </div>
+        <div className={classes.chatsListContainer}>
+          <List className={classes.chatsList}>
+            {chatLists}
+          </List>
+        </div>
+        <div className={classes.chatLayoutContainer}>
+          <List>
+            {messageLists}
+          </List>
+        </div>
+        <div className={classes.bottomNavigationContainer}>
+          <BottomNavigation showLabels className={classes.bottomNav}>
+            <BottomNavigationAction label="My Chats" icon={<RestoreIcon/>}/>
+            <BottomNavigationAction label="Explore" icon={<ExploreIcon/>}/>
+          </BottomNavigation>
+        </div>
+        <div className={classes.inputContainer}>
+          input
         </div>
       </div>
     );
   }
 }
 
-
-export default withStyles(styles)(PermanentDrawer);
+export default withStyles(styles)(App);
